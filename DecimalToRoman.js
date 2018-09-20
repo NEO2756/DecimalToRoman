0014a83ada1b2fc19080912eae9569134744a1b3
Sandeep Sharma <sandeep.jack2756@gmail.com>
2018-09-20 18:11:08 +0530

    Naive roman number implementation

---
+++ DecimalToRoman.js
@@ -0,0 +1,20 @@
+function convertToRoman(num) {
+  var roman = { M: 1000, CM: 900, D: 500, CD: 400, C: 100,XC: 90, L: 50,XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1
+  };
+  var output = '';
+
+  for (var i of Object.keys(roman)) {
+
+
+    var q = Math.floor(num / roman[i]);
+   // If q > 1, num has that many keys
+   //if q == 0, num does not contain this roman 
+    num -= q * roman[i];
+    //Add the number of romans, q times.
+    str += i.repeat(q);
+  }
+
+  return str;
+}
+
+console.log(convertToRoman(1954));
